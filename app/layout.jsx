e19c54221b1417aca4698039c26491e6e3220dcd
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-02-12 22:03:24 -0600

    Components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,10 @@
-import Link from "next/link";
+import Navbar from "../components/Navbar";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <nav>
-          <h1>Navbar</h1>
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/store">Store</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
         {children}
       </body>
     </html>
