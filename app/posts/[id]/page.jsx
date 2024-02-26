08547e75a907d03d37c2cd5953c017249cda9d5c
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-02-26 14:13:15 -0600

    Suspense

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,18 +1,22 @@
+import PostPage from "../page";
+import { Suspense } from "react";
 async function loadPost(id) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
   const data = await res.json();
   return data;
 }
-
 export default async function Page({ params }) {
   const post = await loadPost(params.id);
-
   return (
     <div>
       <h1>
         {post.id} {post.title}
       </h1>
       <p>{post.body}</p>
+      <h3>Related Articles</h3>
+      <Suspense fallback={<div>Loading posts...</div>}>
+        <PostPage />
+      </Suspense>
     </div>
   );
 }
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,9 +1,11 @@
 import PostCard from "@/components/PostCard";
+
+
 async function loadPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
 
-//await new Promise((resolve) => setTimeout(resolve, 3000));
+await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return data;
 }
